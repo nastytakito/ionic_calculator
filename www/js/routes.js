83a8e6f8e26a997215bd9ef5d7537c46c7b64813
Jesús Mario Martínez Morales <jesusmariomtzm@gmail.com>
2017-09-13 12:29:24 -0700

    Add functionality for basic math

---
+++ www/js/routes.js
@@ -0,0 +1,27 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('calculator', {
+    url: '/page1',
+    templateUrl: 'templates/calculator.html',
+    controller: 'calculatorCtrl'
+  })
+
+  .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'templates/menu.html',
+    controller: 'menuCtrl'
+  })
+
+$urlRouterProvider.otherwise('/page1')
+
+
+});
